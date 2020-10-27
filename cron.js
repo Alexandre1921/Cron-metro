c06e69693d5fc2193b2be7f5b5239f5a16f3468e
Alexandre1921 <alexandre_leonardo2010@hotmail.com>
2020-10-27 20:38:52 -0300

    Cronômetro dedbugado

--- cron.js
+++ cron.js
@@ -1,60 +1,76 @@
 "use strict"
 
 var mm = 0;
-var ss = 5;
+var ss = 0;
 
-var tempo = 1000;//Quantos milésimos valem 1 segundo?
+var tempo = 1000; //Quantos milésimos valem 1 segundo?
 var cron;
 const MM = document.getElementById('mm');
 const SS = document.getElementById('ss');
-    
-MM.addEventListener("keyup", ()=>{ mm=MM.value  });
-SS.addEventListener("keyup", ()=>{ ss=SS.value });
 
-var iniciou=true;
+function doChanges() {
+    if (iniciou) {
+        mm = MM.value || 0;
+        if (ss < 0) {
+            ss = 0;
+        }
+        document.getElementById('counter').innerText = (MM.value || 0) + ':' + (ss < 10 ? '0' + ss : ss);
+    }
+}
+MM.addEventListener("keyup", () => {
+    doChanges();
+});
+
+MM.addEventListener("keydown", () => {
+    doChanges();
+});
+
+var iniciou = true;
 //Inicia o temporizador
 function start() {
-    if (iniciou){
+    if (iniciou) {
+        iniciou = false;
         cron = setInterval(() => { timer(); }, tempo);
     }
-    iniciou=false;
 }
 
-//Para o temporizador mas não limpa as variáveis
-function pause() {
-    iniciou=true;
-    clearInterval(cron);
+function reset() {
+    stop();
+    iniciou = true;
 }
 
 //Para o temporizador e limpa as variáveis
 function stop() {
     clearInterval(cron);
 
-    mm = 0;
+    mm = MM.value || 0
     ss = 0;
 
-    document.getElementById('counter').innerText = '00:00';
+    document.getElementById('counter').innerText = (MM.value || 0) + ':' + (ss < 10 ? '0' + ss : ss);
 }
 
 //Faz a contagem do tempo e exibição
 function timer() {
-    ss--; //Incrementa +1 na variável ss
-
     if (ss <= 0) { //Verifica se deu 59 segundos
         ss = 59; //Volta os segundos para 0
         mm--; //Adiciona +1 na variável mm
 
-        if (mm <= 0) { //Verifica se deu 59 minutos
-            mm = 59;//Volta os minutos para 0
-            stop();
+        if (mm < 0) { //Verifica se deu 59 minutos
+            console.log('b');
+            MM.value = 0;
+            reset();
         }
+
     }
 
     //Cria uma variável com o valor tratado MM:SS
-    var format = (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
-    
+    var format = mm + ':' + (ss < 10 ? '0' + ss : ss);
+
     //Insere o valor tratado no elemento counter
     document.getElementById('counter').innerText = format;
+
+
+    ss--; //Incrementa +1 na variável ss
 
     //Retorna o valor tratado
     return format;
